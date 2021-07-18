f6a452dec5893f1689147b5b8686784ca0abb276
Durgesh Kumar <durgeshkumar384@gmail.com>
2021-07-18 14:25:05 +0530

    first commit

---
+++ lib/custom/OptionButton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate/utils/constants.dart';
+import 'package:real_estate/utils/widget_functions.dart';
+
+class OptionButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final double width;
+
+  const OptionButton(
+      {Key? key, required this.text, required this.icon, required this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      child: FlatButton(
+        minWidth: width,
+        color: COLOR_DARK_BLUE,
+        splashColor: COLOR_WHITE.withAlpha(55),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        onPressed: () {},
+        child: Row(children: [
+          Icon(icon, color: COLOR_WHITE),
+          addHorizontalSpace(10),
+          Text(text, style: TextStyle(color: COLOR_WHITE))
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate/screens/LandingPage.dart';
+import 'dart:ui';
+
+import 'utils/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Design 1',
+      theme: ThemeData(primaryColor: COLOR_WHITE, accentColor: COLOR_DARK_BLUE, textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT, fontFamily: "Montserrat"),
+      home: LandingScreen(),
+    );
+  }
+}
